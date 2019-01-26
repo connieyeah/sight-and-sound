6284080fc2dba6531fc0dabbc5691c175c08af7a
Kevin Chin <chinkevin25@gmail.com>
2019-01-26 11:08:06 -0800

    add prop-types

--- client/src/components/Topic.jsx
+++ client/src/components/Topic.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Topic = ({ topic }) => {
-  return <h1>{topic}</h1>;
+const Topic = ({ topic }) => <h1>{topic}</h1>;
+
+Topic.propTypes = {
+  topic: PropTypes.string.isRequired,
 };
 
 export default Topic;
