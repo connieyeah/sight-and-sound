c9e29a4bcc68952ecc8f4a7110a0905cec6fcf21
Kevin Chin <chinkevin25@gmail.com>
2019-01-26 14:24:44 -0800

    Revert "Render seven"

--- client/src/index.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-env browser */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import WritingPrompt from './components/WritingPrompt';
-
-ReactDOM.render(<WritingPrompt />, document.getElementById('writing-prompt'));
