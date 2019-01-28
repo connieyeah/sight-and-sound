e512c15535801a3c4005747da3e21b9656b8f077
Connie Ea <connieyeah@gmail.com>
2019-01-28 01:53:33 -0800

    login to spotify

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      urlSearchInput: ''
+      urlSearchInput: '',
+      dataFromClarifi: null
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
@@ -14,20 +18,40 @@ class Search extends React.Component {
 
   // function that sends url to clarifai api
   handleSubmit() {
+    const { urlSearchInput } = this.state
+    console.log('photo to be searched', this.state.urlSearchInput)
+
+    axios.post('/clarifai', {
+      urlSearchInput,
+    })
+    .then((response) => {
+      // This response should have data from Clarifi Api
+      // Save the data to state if needed
+      // Clean up the data to a format that Spotify may use
+      // Make another API to call to Spotify
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
 
 
   render() {
-    console.log(`search component being rendered...`, this.props)
     return (
       <div>
         <h1>SEARCH</h1>
         <form>
           <label>
             Enter image url
-            <input type="text" name="" />
+            <input 
+              type="text"
+              name=""
+              onChange={this.handleChange}
+            />
           </label>
         </form>
+        <button onClick={this.handleSubmit}>Submit</button>
       </div>
       
     )
